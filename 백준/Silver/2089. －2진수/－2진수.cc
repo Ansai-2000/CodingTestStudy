b15a0beb390176ebbbd1bd01811bb94171d6c91a
Ansai-2000 <dudgns3681@naver.com>
2023-04-05 16:18:24 +0900

    [Silver II] Title: -2진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2089. －2진수/－2진수.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    int n;
+    cin >> n;
+    string str;
+    if(n==0){
+        cout <<'0';
+        return 0;
+    }
+    while(n!=0){
+        if(n%-2==0){
+            str+='0';
+            n/=-2;
+        }
+        else{
+            str+='1';
+            n=(n-1)/-2;
+        }
+
+    }
+    reverse(str.begin(),str.end());
+    cout << str;
+
+}
