dd09c1cbd6ab0fe5cbaa7ded80db5b75cfe12c81
Ansai-2000 <dudgns3681@naver.com>
2023-04-06 21:51:57 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#include <queue>
+
+int main() {
+
+    deque<int> q;
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        q.push_back(i);
+    }
+    while(q.size()!=1){
+        cout << q.front() << ' ';
+        q.pop_front();
+        q.push_back(q.front());
+        q.pop_front();
+
+    }
+    cout << q.front() << endl;
+}
