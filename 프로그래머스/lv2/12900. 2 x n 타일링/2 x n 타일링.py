a3446460a42db101043e70b79929335df8e617f1
Ansai-2000 <dudgns3681@naver.com>
2023-08-02 15:32:41 +0900

    [level 2] Title: 2 x n 타일링, Time: 19.49 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    dp = [0] * 60001
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+    dp[4] = 5
+    for i in range(3,n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+    return dp[n]
