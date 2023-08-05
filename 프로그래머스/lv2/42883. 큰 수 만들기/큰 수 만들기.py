d0d7002e3b2c1caba28b211e68785710ff7a65ec
Ansai-2000 <dudgns3681@naver.com>
2023-08-05 14:20:12 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,11 @@
+def solution(number, k):
+    answer = ''
+    st = []
+    for num in number:
+        while st and st[-1] < num and k > 0:
+            st.pop()
+            k-=1
+        st.append(num)    
+    if k>0:
+        st = st[:-k]
+    return "".join(st)
