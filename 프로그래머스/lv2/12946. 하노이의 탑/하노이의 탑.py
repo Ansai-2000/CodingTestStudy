192756c761601b4526470e145415328bf6824c11
Ansai-2000 <dudgns3681@naver.com>
2023-08-16 14:28:58 +0900

    [level 2] Title: 하노이의 탑, Time: 12.68 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = []
+    def hanoi(a,b,c,n):
+        if n == 1:
+            answer.append([a,c])
+        else:
+            hanoi(a,c,b,n-1)
+            hanoi(a,b,c,1)
+            hanoi(b,a,c,n-1)   
+    hanoi(1,2,3,n)    
+    return answer
